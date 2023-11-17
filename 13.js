9892ef8be0864fbb4f9eab0a05d0f4ce00b7b476
mrinal1224 <iammrinalkc@gmail.com>
2023-11-17 16:05:16 +0530

    fixed 13

--- 13.js
+++ 13.js
@@ -1,28 +1,24 @@
-// Test Data :
-// console.log(first([7, 9, 0, -2]));
-// console.log(first([],3));
-// console.log(first([7, 9, 0, -2],3));
-// console.log(first([7, 9, 0, -2],6));
-// console.log(first([7, 9, 0, -2],-3));
-// Expected Output :
-// 7
-// []
-// [7, 9, 0]
-// [7, 9, 0, -2]
-// []
+// Problem Statement: Implement a memoization function.
 
-var first =  function(array, n) {
-    if (array == null) 
-    return void 0;
-  if (n == null) 
-    return array[0];
-  if (n < 0)
-    return [];
-  return array.slice(0, n);
-};
+// Description: Write a function that takes a function as its argument and returns a memoized version of the function.
 
-console.log(first([7, 9, 0, -2]));
-console.log(first([],3));
-console.log(first([7, 9, 0, -2],3));
-console.log(first([7, 9, 0, -2],6));
-console.log(first([7, 9, 0, -2],-3));
+// Solution Approach: Store the function's results for previous inputs and return the stored result for repeated inputs.
+
+// Solution
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    const key = JSON.stringify(args);
+    if (!cache[key]) {
+      cache[key] = fn.apply(this, args);
+    }
+    return cache[key];
+  };
+}
+
+
+// Test Cases
+
+// const fib = memoize(n => n <= 1 ? n : fib(n - 1) + fib(n - 2));
+// console.log(fib(10)); // 55
+
